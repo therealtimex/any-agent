8bee94f32a39e3ebb42fcfc562b03af6a5a209f4
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2025-04-15 14:56:28 +0200

    tests: Add autouse fixture `disable_rich_console`. (#85)
    * tests: Add autouse fixture `disable_rich_console`.
    
    * tests: Disable `rich.console`  only if `pytestconfig.option.capture != "no"`.
    
    Allows to see rich output when running with `pytest -s`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import sys
 
 import pytest
+import rich.console
 
 
 @pytest.fixture(scope="function")
@@ -19,3 +20,15 @@ def refresh_tools():
             tool_modules.append(k)
     for module in tool_modules:
         del sys.modules[module]
+
+
+@pytest.fixture(autouse=True)
+def disable_rich_console(monkeypatch, pytestconfig):
+    original_init = rich.console.Console.__init__
+
+    def quiet_init(self, *args, **kwargs):
+        if pytestconfig.option.capture != "no":
+            kwargs["quiet"] = True
+        original_init(self, *args, **kwargs)
+
+    monkeypatch.setattr(rich.console.Console, "__init__", quiet_init)
