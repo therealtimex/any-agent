eb27b5d447716edb99e969410c0f13bbcbc8c870
Hareesh <hareeshbahuleyan@gmail.com>
2025-06-18 14:48:40 +0200

    Make any_agent.__version__ attribute available (#491)
    * Make any_agent.__version__ attribute available
    
    * Using importlib.metadata
    
    * Reorder imports
    
    * Change unknown to dev

--- src/any_agent/__init__.py
+++ src/any_agent/__init__.py
@@ -1,6 +1,15 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .config import AgentConfig, AgentFramework
 from .frameworks.any_agent import AgentRunError, AnyAgent
 from .tracing.agent_trace import AgentTrace
+
+try:
+    __version__ = version("any-agent")
+except PackageNotFoundError:
+    # In the case of local development
+    # i.e., running directly from the source directory without package being installed
+    __version__ = "0.0.0-dev"
 
 __all__ = [
     "AgentConfig",
@@ -8,4 +17,5 @@ __all__ = [
     "AgentRunError",
     "AgentTrace",
     "AnyAgent",
+    "__version__",
 ]
