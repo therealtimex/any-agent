60076527b5cc7fa3e7d19373fc5988c6e6c292d9
daavoo <daviddelaiglesiacastro@gmail.com>
2025-09-01 15:42:47 +0200

    fix(demo): Update `repo_id` in  `restart_space`.

--- demo/restart_space.py
+++ demo/restart_space.py
@@ -5,7 +5,7 @@ from huggingface_hub import HfApi
 if __name__ == "__main__":
     api = HfApi()
     api.restart_space(
-        repo_id="mozilla-ai/document-to-podcast",
+        repo_id="mozilla-ai/any-agent-demo",
         token=os.getenv("HF_TOKEN"),
         factory_reboot=True,
     )
